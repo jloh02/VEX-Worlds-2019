6a54fce2d5cde52f7b644b7026d920bb5cc39e9d
Jonathan Loh <jonathanloh02@gmail.com>
2019-04-15 10:40:48 +0800

    Updated skills route

--- src/real_skills.cpp
+++ src/real_skills.cpp
@@ -39,23 +39,18 @@ void real_skills(){
 
   baseTurn(-3,0.48,0.5);  //Turn to face low flags
   waitBase(3000);
+  setLift(0);
   delay(100);
   baseMove(91,0.53,0.2);  //Score low flags
   while(getY()<0) delay(25); //Shoot high flags while going forward to hit low flag
   catapultActivated = true; //Score 2 high flags
   waitBase(3000);
   delay(200);
-  baseMove(-52,0.56,0.2);  //Return to shooting position
-  waitBase(3000);
-  baseTurn(2,3.0,0.0);  //Turn to face high flags
-  waitBase(3000);
 
-  intake.move(0);
-  delay(300);
-
+  //////////FROM HERE: IT IS AUTONOMOUS
+  baseMove(-40.5,0.56,0.2);  //Get into position to get balls from cap
+  waitBase(3000);
   setPusher(250);   //Pusher in position to get balls from cap
-  baseMove(11.5,0.5,0.2);    //Position to get good angle to get balls from cap
-  waitBase(3000);
   baseTurn(45, 0.6, 0.2);   //Face cap
   waitBase(3000);
   baseMove(17, 0.45, 0.3);  //Move towards cap
