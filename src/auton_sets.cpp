c60a5c79572db93818eba9c7f2887369ed4a352f
TheDinos <marcussvps2002@gmail.com>
2019-04-18 08:19:05 +0800

    added the back 2 autons

--- src/auton_sets.cpp
+++ src/auton_sets.cpp
@@ -1,5 +1,131 @@
 #include "auton_sets.hpp"
 
+void backRed(){
+  Motor lift(liftPort);
+  Motor intake(intakePort);
+
+  setPusher(20);  //Pusher in position to hit low flag
+
+  resetCoord(4,-51.3,90);
+  intake.move(100);  //start intake
+  baseMove(34.5,0.55,0.1); //Move forward to collect ball
+  waitBase(3000);
+  delay(200);
+  baseMove(-10, 0.59, 0.0);    // move back to position for the turn to the cap
+  waitBase(3000);
+  intake.move(0);
+
+  baseTurn(-38,0.5,0.15); //Back face cap
+  waitBase(3000);
+
+  baseMove(-18.5, 0.6, 0.05); // Move back to Intake cap
+  waitBase(3000);
+//  delay(500);
+  setLift(200);    //raise the cap
+  delay(200);
+
+  baseMove(7, 0.64,0.0); //Position to score on post
+  waitBase(10000);
+
+  baseTurn(-90,0.9,0.0);
+//  baseTurn(-12,-56.8,0.6,0.0); //Face post
+  waitBase(3000);
+
+  pausePusher(true);
+  baseMove(36.0, 0.38, 0.1);     //Move to Score cap
+  waitBase(10000);
+
+  setLift(515);                //score cap
+  double startScore = millis();
+  while(fabs(515 - lift.get_position()) > 5 && millis()-startScore<1300) {
+    delay(25);
+  }
+  baseMove(-7, 0.5, 0.0);      //Move away from wall
+  waitBase(3000);
+
+  pausePusher(false);         //remove it from the slack position
+  setLift(0);                  //put lift down
+  delay(500);                  //wait for lift to come down
+
+  baseTurn(-5,0.48,0.2);       //Turn to face low flags
+  waitBase(3000);
+  delay(100);
+  baseMove(30,0.55,0.1);        //move to the parking platform
+  waitBase(3000);
+  baseTurn(-5,49.5,2.0,0.1);      //turn to Score high flags
+  waitBase(3000);
+  catapultActivated = true;    //Score 2 high flags
+  delay(200);
+  baseTurn(90,0.5,0.0);      //turn to climb
+  waitBase(3000);
+  baseMove(34,0.55,0.0);       // climb
+  waitBase(3000);
+
+}
+void backBlue(){
+  Motor lift(liftPort);
+  Motor intake(intakePort);
+
+  setPusher(20);  //Pusher in position to hit low flag
+
+  resetCoord(4,-51.3,-90);
+  intake.move(100);  //start intake
+  baseMove(34.5,0.55,0.1); //Move forward to collect ball
+  waitBase(3000);
+  delay(200);
+  baseMove(-10, 0.59, 0.0);    // move back to position for the turn to the cap
+  waitBase(3000);
+  intake.move(0);
+
+  baseTurn(38,0.5,0.15); //Back face cap
+  waitBase(3000);
+
+  baseMove(-18.5, 0.6, 0.05); // Move back to Intake cap
+  waitBase(3000);
+//  delay(500);
+  setLift(200);    //raise the cap
+  delay(200);
+
+  baseMove(7, 0.64,0.0); //Position to score on post
+  waitBase(10000);
+
+  baseTurn(90,0.9,0.0);
+//  baseTurn(-12,-56.8,0.6,0.0); //Face post
+  waitBase(3000);
+
+  pausePusher(true);
+  baseMove(36.0, 0.38, 0.1);     //Move to Score cap
+  waitBase(10000);
+
+  setLift(515);                //score cap
+  double startScore = millis();
+  while(fabs(515 - lift.get_position()) > 5 && millis()-startScore<1300) {
+    delay(25);
+  }
+  baseMove(-7, 0.5, 0.0);      //Move away from wall
+  waitBase(3000);
+
+  pausePusher(false);         //remove it from the slack position
+  setLift(0);                  //put lift down
+  delay(500);                  //wait for lift to come down
+
+  baseTurn(5,0.48,0.2);       //Turn to face low flags
+  waitBase(3000);
+  delay(100);
+  baseMove(30,0.55,0.1);        //move to the parking platform
+  waitBase(3000);
+  baseTurn(-5,49.5,2.0,0.1);      //turn to Score high flags
+  waitBase(3000);
+  catapultActivated = true;    //Score 2 high flags
+  delay(200);
+  baseTurn(-90,0.5,0.0);      //turn to climb
+  waitBase(3000);
+  baseMove(34,0.55,0.0);       // climb
+  waitBase(3000);
+
+}
+
+
 void redCore(){
   Motor intake(intakePort);
   resetCoord(0, 0, 90);
